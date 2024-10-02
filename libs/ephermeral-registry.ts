1fc87c728def038b884c63c68b668d9098b6bc24
Muhammad Ilham <muhammad.ilhamsa1@gmail.com>
2024-10-02 16:01:18 +0700

    publish registry abi

---
+++ libs/ephermeral-registry.ts
@@ -0,0 +1,29 @@
+import { createWalletClient, createPublicClient, custom, http } from 'viem';
+import { sepolia } from 'viem/chains';
+import { privateKeyToAccount } from 'viem/accounts';
+import ERC6538RegistryABI from './abi/ERC6538RegistryABI'; // Your ABI
+
+const client = createPublicClient({
+  chain: sepolia,
+  transport: http(),
+});
+
+const walletClient = createWalletClient({
+  account: privateKeyToAccount(`0x${process.env.EXPO_PUBLIC_PRIVATE_KEY}`),
+  chain: sepolia,
+  transport: http(),
+});
+
+const contractAddress = '0xYourContractAddressHere';
+
+export async function registerKeysOnBehalf(registrant: string, schemeId: number, signature: string, stealthMetaAddress: string) {
+  const hash = await walletClient.writeContract({
+    address: contractAddress,
+    abi: ERC6538RegistryABI,
+    functionName: 'registerKeysOnBehalf',
+    args: [registrant, schemeId, signature, stealthMetaAddress],
+  });
+
+  console.log('Transaction hash:', hash);
+}
+
